b9c89a6b356a13ce6030409825d651acac8ae774
Seyam Bhuyan <bhuyanseyam4@gmail.com>
2026-03-08 02:43:13 +0600

    Submit Array - pallindrom by AFRUJA AKTER MOON

---
+++ students/3002_AFRUJA AKTER MOON/Array/pallindrom/delete_element.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+   int n;
+   scanf("%d", &n);
+   int arr[n];
+   for(int i=0;i<n;i++){
+    scanf("%d", &arr[i]);
+   }
+   int index;
+   scanf("%d", &index);
+
+   for(int i=index;i<n-1;i++)
+   {
+        arr[i]=arr[i+1];
+   }
+n--;
+   for(int i=0;i<n;i++){
+    printf("%d ", arr[i]);
+   }
+    return 0;
+}
