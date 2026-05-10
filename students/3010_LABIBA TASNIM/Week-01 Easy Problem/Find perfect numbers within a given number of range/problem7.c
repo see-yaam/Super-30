f1004cd88ea90731dcc5325ee45a581b9978cf91
Seyam Bhuyan <bhuyanseyam4@gmail.com>
2026-05-11 02:50:39 +0600

    Submit Week-01 Easy Problem - Find perfect numbers within a given number of range by LABIBA TASNIM

---
+++ students/3010_LABIBA TASNIM/Week-01 Easy Problem/Find perfect numbers within a given number of range/problem7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int start,end,i,j,sum;
+    printf("Enter starting value: ");
+    scanf("%d",&start);
+
+    printf("Enter ending value: ");
+    scanf("%d",&end);
+
+    for(i=start;i<=end;i++)
+    {
+        sum=0;
+        for(j=1;j<i;j++)
+        {
+            if(i%j==0)
+            {
+                sum=sum+j;
+            }
+        }
+        if(sum==i && i!=0)
+        {
+            printf("%d ",i);
+        }
+    }
+
+    return 0;
+}
