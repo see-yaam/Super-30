48bf83a09846e9debd9705f90088a1c679270eef
Seyam Bhuyan <bhuyanseyam4@gmail.com>
2026-05-11 23:44:06 +0600

    Submit Week-01 Easy Problem - Display Pascal's triangle by MIR AFTAB UDDIN

---
+++ students/3015_MIR AFTAB UDDIN/Week-01 Easy Problem/Display Pascal's triangle/10.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    int no_row, c = 1, blk, i, j;
+
+    cout << "Input number of rows: ";
+    cin >> no_row;
+
+    for (i = 0; i < no_row; i++) {
+
+        for (blk = 1; blk <= no_row - i; blk++) {
+            cout << "  ";
+        }
+
+        for (j = 0; j <= i; j++) {
+
+            if (j == 0 || i == 0)
+                c = 1;
+            else
+                c = c * (i - j + 1) / j;
+
+            cout << setw(4) << c;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
